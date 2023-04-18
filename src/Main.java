354e3fe5aa57d88965c201ba436de57f2e5336bd
Devaiah <devaih.100@gmail.com>
2023-04-18 11:48:22 +0530

    Updated hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Hi this is Devaiah");
+        System.out.println("Hello world! Hi this is Devaiah from DSU");
     }
 }
